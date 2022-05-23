969519169b1f4c76fcf7655f9bcc65ef8927c4d9
Alexey <gaprix.dev@outlook.com>
2022-05-23 22:14:25 +0300

    First version

---
+++ src/Gaprix/FixPlayerDuplication/FixPlayerDuplication.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Gaprix\FixPlayerDuplication;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerKickEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\protocol\LoginPacket;
+use pocketmine\plugin\PluginBase;
+
+class FixPlayerDuplication extends PluginBase implements Listener
+{
+
+	public $onlinePlayers = [];
+
+	public function onEnable()
+	{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	public function onDataPacketReceive(DataPacketReceiveEvent $event)
+	{
+		$packet = $event->getPacket();
+		$player = $event->getPlayer();
+		if($packet instanceof LoginPacket) {
+			foreach($this->onlinePlayers as $existingPlayer) {
+				if(strcasecmp($existingPlayer->getName(), $packet->username) === 0 || $existingPlayer->getUniqueId()->equals($packet->clientUUID)) {
+					$ev = new PlayerDuplicateLoginEvent($player, $existingPlayer);
+					$this->getServer()->getPluginManager()->callEvent($ev);
+					if($ev->isCancelled()){
+						$player->kick($ev->getDisconnectMessage());
+						return;
+					}
+					$existingPlayer->kick($ev->getDisconnectMessage());
+					unset($this->onlinePlayers[$existingPlayer->getName()]);
+				}
+			}
+			$this->onlinePlayers[$packet->username] = $player;
+		}
+	}
+
+	public function onKick(PlayerKickEvent $event)
+	{
+		unset($this->onlinePlayers[$event->getPlayer()->getName()]);
+	}
+
+	public function onDisconnect(PlayerQuitEvent $event)
+	{
+		unset($this->onlinePlayers[$event->getPlayer()->getName()]);
+	}
+
+}
